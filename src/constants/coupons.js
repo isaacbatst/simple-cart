840175e9fe734786561d44fb4d49753b1d550925
Isaac Batista <isaacbatst@gmail.com>
2020-01-13 20:26:54 -0300

    feat: add coupons constants

---
+++ src/constants/coupons.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, name: "A", type: "percentual", rule: 30 },
+  { id: 2, name: "FOO", type: "fixed", rule: 100 },
+  { id: 3, name: "C", type: "freeShipping", rule: 300.5},
+]
