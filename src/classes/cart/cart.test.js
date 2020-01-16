37de0f0f8146bd07c36c068718c25e607e3c17e2
Isaac Batista <isaacbatst@gmail.com>
2020-01-15 22:23:15 -0300

    test: add percentual discount testing

--- src/classes/cart/cart.test.js
+++ src/classes/cart/cart.test.js
@@ -1,25 +1,47 @@
 import Cart from "./";
 import products from '../../constants/products'
+import coupons from '../../constants/coupons';
+
 import { INITIAL_STATE } from "../../store/reducers/cart";
 
-describe("test shipping price dynamic", () => {
-  const cart = Cart(INITIAL_STATE);
+const emptyCart = Cart(INITIAL_STATE);
+
+describe("test shipping price", () => {
 
   it("should return a shipping price of 30", () => {
-    const updatedCart = cart.updateCart({ product: products[0], newQuantity: 3 });
+    const updatedCart = emptyCart.updateCart({ product: products[0], newQuantity: 3 });
     expect(updatedCart.values.shippingPrice).toBe(30);
   });
 
   it("should return a free shipping price", () => {
-    const updatedCart = cart.updateCart({ product: products[0], newQuantity: 41 });
+    const updatedCart = emptyCart.updateCart({ product: products[0], newQuantity: 41 });
     expect(updatedCart.values.shippingPrice).toBe(0);
   })
 
   it("should return a shipping price based on how many 5kg above 10kg", () => {
-    const updatedCartWithFiveMutipleKg = cart.updateCart({ product: products[0], newQuantity: 20 });
+    const updatedCartWithFiveMutipleKg = emptyCart.updateCart({ product: products[0], newQuantity: 20 });
     expect(updatedCartWithFiveMutipleKg.values.shippingPrice).toBe(44);
 
-    const updatedCartWithoutFiveMutipleKg = cart.updateCart({product: products[1], newQuantity: 17})
+    const updatedCartWithoutFiveMutipleKg = emptyCart.updateCart({product: products[1], newQuantity: 17})
     expect(updatedCartWithoutFiveMutipleKg.values.shippingPrice).toBe(37);
   })
 });
+
+describe("test adding coupon", () => {
+  const stateWithItems = emptyCart.updateCart({ product: products[0], newQuantity: 20 });
+  
+  const [percentual, fixed, freeShipping ] = coupons;
+
+  it("should return correct values after adding percentual discount", () => {
+    const cart = Cart(stateWithItems);
+    const { values: { total, subtotal, shippingPrice } }  = cart.applyCoupon(percentual);
+    const { rule } = percentual;
+
+    expect(total).toBe(subtotal - calcSubtotalPercentage(rule, subtotal) + shippingPrice);
+
+    function calcSubtotalPercentage(rule, subtotal){
+      return (rule / 100) * subtotal;
+    }
+  })
+
+})
